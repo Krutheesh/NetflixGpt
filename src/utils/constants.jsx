0c09fcbbdfc34b27719126cb977dade9da58827f
krutheesh <krutheeshsiripuram@gmail.com>
2025-02-12 10:00:03 +0530

    open api k changed to K

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,12 +1,11 @@
-
 export const API_OPTIONS = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: import.meta.env.VITE_APP_TMDB_KEY
+    accept: "application/json",
+    Authorization: import.meta.env.VITE_APP_TMDB_KEY,
   },
-  timeout: 50
+  timeout: 50,
 };
-export const IMG_CDN_URL = import.meta.env.VITE_APP_IMG_CDN_URL
+export const IMG_CDN_URL = import.meta.env.VITE_APP_IMG_CDN_URL;
 
-export const OPEN_AI_KEY = import.meta.env.VITE_APP_OPENAI_kEY
+export const OPEN_AI_KEY = import.meta.env.VITE_APP_OPENAI_KEY;
